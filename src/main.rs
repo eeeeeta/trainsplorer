f9ee73b2d1cba4a8e7499a9c8c9b22c38f362a8b
eeeeeta <eeeeeta@users.noreply.github.com>
2017-08-15 12:29:13 +0100

    Station routing, yay

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use std::collections::{HashSet, BTreeSet, HashMap};
 use postgres::{Connection, GenericConnection, TlsMode};
 use postgres::rows::Row;
 use postgres::types::ToSql;
-use postgis::ewkb::{Point, LineString, LineStringT};
+use postgis::ewkb::{Point, LineString, Geometry, LineStringT, Polygon, GeometryCollection};
 use osm_signal::*;
 use ordered_float::OrderedFloat;
 use indicatif::ProgressBar;
@@ -93,11 +93,59 @@ parent_geom geometry
             parent_geom: row.get(6)
         }
     }
-    pub fn insert<T: GenericConnection>(conn: &T, location: Point, distance: f32) -> Result<()> {
-        conn.execute("INSERT INTO nodes (location, distance, parent, visited) VALUES ($1, $2) ON CONFLICT DO NOTHING",
-                     &[&location, &distance])?;
+    pub fn insert<T: GenericConnection>(conn: &T, location: Point) -> Result<i32> {
+        let qry = conn.query("INSERT INTO nodes (location) VALUES ($1) ON CONFLICT DO NOTHING RETURNING id",
+                             &[&location])?;
+        let mut ret = None;
+        for row in &qry {
+            ret = Some(row.get(0))
+        }
+        if ret.is_none() {
+            for row in &conn.query("SELECT id FROM nodes WHERE location = $1", &[&location])? {
+                ret = Some(row.get(0))
+            }
+        }
+        Ok(ret.expect("Somehow, we never got an id in Node::insert..."))
+    }
+}
+#[derive(Debug, Clone)]
+pub struct Station {
+    nr_ref: String,
+    point: i32,
+    area: Polygon
+}
+impl Station {
+    pub fn make_table<T: GenericConnection>(conn: &T) -> Result<()> {
+        conn.execute(r#"
+CREATE TABLE IF NOT EXISTS stations (
+nr_ref VARCHAR UNIQUE NOT NULL,
+point INT NOT NULL,
+area geometry NOT NULL
+);"#, &[])?;
         Ok(())
     }
+    pub fn from_select<T: GenericConnection>(conn: &T, where_clause: &str, args: &[&ToSql]) -> Result<Vec<Self>> {
+        let query = format!("SELECT * FROM stations {}", where_clause);
+        let qry = conn.query(&query, args)?;
+        let mut ret = vec![];
+        for row in &qry {
+            ret.push(Self::from_row(&row));
+        }
+        Ok(ret)
+    }
+    pub fn from_row(row: &Row) -> Self {
+        Self {
+            nr_ref: row.get(0),
+            point: row.get(1),
+            area: row.get(2),
+        }
+    }
+    pub fn insert<T: GenericConnection>(conn: &T, nr_ref: &str, point: i32, area: Polygon) -> Result<()> {
+        conn.execute("INSERT INTO stations (nr_ref, point, area) VALUES ($1, $2, $3)",
+                     &[&nr_ref, &point, &area])?;
+        Ok(())
+    }
+
 }
 #[derive(Debug, Clone)]
 pub struct Link {
@@ -117,7 +165,7 @@ distance REAL NOT NULL
 );"#, &[])?;
         Ok(())
     }
-        pub fn from_select<T: GenericConnection>(conn: &T, where_clause: &str, args: &[&ToSql]) -> Result<Vec<Self>> {
+    pub fn from_select<T: GenericConnection>(conn: &T, where_clause: &str, args: &[&ToSql]) -> Result<Vec<Self>> {
         let query = format!("SELECT p1, p2, way, distance FROM links {}", where_clause);
         let qry = conn.query(&query, args)?;
         let mut ret = vec![];
@@ -147,11 +195,71 @@ fn count<T: GenericConnection>(conn: &T, details: &str, args: &[&ToSql]) -> Resu
         .ok_or("Count query failed")?
         .get(0))
 }
+fn new_node_at_point<T: GenericConnection>(conn: &T, point: Point) -> Result<i32> {
+    let trans = conn.transaction()?;
+    let links = Link::from_select(&trans, "WHERE ST_Intersects(way, $1) AND NOT ST_Intersects(ST_EndPoint(way), $1) AND NOT ST_Intersects(ST_StartPoint(way), $1)", &[&point])?;
+    if links.len() > 1 {
+        bail!("There are >1 links containing that point! Something is broken.");
+    }
+    let node = Node::insert(&trans, point.clone())?;
+    for link in links {
+        //println!("[+] Splitting link {} <-> {}", link.p1, link.p2);
+        for row in &trans.query("SELECT ST_GeometryN(ST_Split($1, $2), 1), ST_GeometryN(ST_Split($1, $2), 2), CAST(ST_Length(ST_GeometryN(ST_Split($1, $2), 1)) AS REAL), CAST(ST_Length(ST_GeometryN(ST_Split($1, $2), 2)) AS REAL)", &[&link.way, &point])? {
+            let (first, last): (LineString, LineString) = (row.get(0), row.get(1));
+            let (df, dl): (f32, f32) = (row.get(2), row.get(3));
+            trans.execute("UPDATE links SET p2 = $1, way = $2, distance = $3 WHERE p1 = $4 AND p2 = $5",
+                         &[&node, &first, &df, &link.p1, &link.p2])?;
+            let new = Link {
+                p1: node,
+                p2: link.p2,
+                distance: dl,
+                way: last
+            };
+            //println!("[+] New setup: {} <-> {} <-> {}", link.p1, node, link.p2);
+            new.insert(&trans)?;
+        }
+    }
+    trans.commit()?;
+    Ok(node)
+}
+fn make_stations<T: GenericConnection>(conn: &T) -> Result<()> {
+    let trans = conn.transaction()?;
+    let mut areas: HashMap<String, (Polygon, Point)> = HashMap::new();
+    for row in &trans.query("SELECT ref, way, ST_Centroid(way) FROM planet_osm_polygon WHERE railway = 'station' AND ref IS NOT NULL", &[])? {
+        areas.insert(row.get(0), (row.get(1), row.get(2)));
+    }
+    for row in &trans.query("SELECT ref, ST_Buffer(way::geography, 50)::geometry, way FROM planet_osm_point WHERE railway = 'station' AND ref IS NOT NULL", &[])? {
+        areas.insert(row.get(0), (row.get(1), row.get(2)));
+    }
+    println!("[+] {} stations to process", areas.len());
+    let bar = ProgressBar::new(areas.len() as _);
+    for (nr_ref, (poly, point)) in areas {
+        let node = new_node_at_point(&trans, point.clone())?;
+        for row in &trans.query("SELECT ST_ShortestLine($1, way), ST_EndPoint(ST_ShortestLine($1, way)) FROM planet_osm_line WHERE railway = 'rail' AND ST_Intersects(way, $2)",
+                                &[&point, &poly])? {
+            let (way, end): (LineString, Point) = (row.get(0), row.get(1));
+            let end = new_node_at_point(&trans, end)?;
+            let link = Link {
+                p1: node,
+                p2: end,
+                distance: 0.0,
+                way: way
+            };
+            link.insert(&trans)?;
+        }
+        Station::insert(&trans, &nr_ref, node, poly)?;
+        bar.inc(1);
+    }
+    bar.finish();
+    trans.commit()?;
+    Ok(())
+}
 fn osm() -> Result<()> {
     let conn = Connection::connect(DATABASE_URL, TlsMode::None)?;
     println!("[+] Creating tables...");
     Node::make_table(&conn)?;
     Link::make_table(&conn)?;
+    Station::make_table(&conn)?;
     let cnt = count(&conn, "FROM planet_osm_line WHERE railway IS NOT NULL", &[])?;
     println!("[+] {} ways in database", cnt);
     let mut nodes = count(&conn, "FROM nodes", &[])?;
@@ -160,8 +268,8 @@ fn osm() -> Result<()> {
         let bar = ProgressBar::new(cnt as _);
         let trans = conn.transaction()?;
         for row in &trans.query("SELECT ST_StartPoint(way), ST_EndPoint(way) FROM planet_osm_line WHERE railway IS NOT NULL", &[])? {
-            Node::insert(&trans, row.get(0), ::std::f32::INFINITY)?;
-            Node::insert(&trans, row.get(1), ::std::f32::INFINITY)?;
+            Node::insert(&trans, row.get(0))?;
+            Node::insert(&trans, row.get(1))?;
             bar.inc(1);
         }
         trans.commit()?;
@@ -186,6 +294,13 @@ fn osm() -> Result<()> {
         links = count(&conn, "FROM links", &[])?;
     }
     println!("[+] {} links in database", links);
+    let mut stations = count(&conn, "FROM stations", &[])?;
+    if stations == 0 {
+        println!("[+] Processing stations...");
+        make_stations(&conn)?;
+        stations = count(&conn, "FROM stations", &[])?;
+    }
+    println!("[+] {} stations in database", stations);
     let unclassed_nodes = count(&conn, "FROM nodes WHERE graph_part = 0", &[])?;
     let mut cur_graph_part = 1;
     if unclassed_nodes > 0 {
@@ -234,8 +349,10 @@ fn osm() -> Result<()> {
     }
     println!("[+] All nodes separated, {} graph parts", cur_graph_part);
     conn.execute("UPDATE nodes SET distance = 'Infinity', visited = false, parent = NULL, parent_geom = NULL", &[])?;
-    const starting_node: i32 = 45884;
-    const goal_node: i32 = 45282;
+    let start = "CLJ";
+    let end = "QRB";
+    let starting_node = Station::from_select(&conn, "WHERE nr_ref = $1", &[&start])?[0].point;
+    let goal_node = Station::from_select(&conn, "WHERE nr_ref = $1", &[&end])?[0].point;
     conn.execute("UPDATE nodes SET distance = 0 WHERE id = $1", &[&starting_node])?;
     let trans = conn.transaction()?;
     println!("[+] Navigating from {} to {}", starting_node, goal_node);
