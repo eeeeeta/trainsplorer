f230aeef678e78fdef8e17a5744f6e9a805d722e
eeeeeta <eeeeeta@users.noreply.github.com>
2017-09-02 17:05:57 +0100

    complete implementation of live data support

--- osms-db/src/ntrod/live.rs
+++ osms-db/src/ntrod/live.rs
@@ -1,14 +1,32 @@
-use ntrod_types::movements::*;
+use ntrod_types::movements::{Activation, Cancellation, Movement, Record, MvtBody};
+use ntrod_types::reference::CorpusEntry;
 use super::types::*;
+use osm::types::*;
 use db::{DbType, InsertableDbType, GenericConnection};
 use errors::*;
 
+pub fn process_ntrod_event<T: GenericConnection>(conn: &T, r: Record) -> Result<()> {
+    let Record { header, body } = r;
+    debug!("Processing message type {} from system {} (source {})",
+           header.msg_type, header.source_system_id, header.original_data_source);
+    match body {
+        MvtBody::Activation(a) => process_activation(conn, a)?,
+        MvtBody::Cancellation(a) => process_cancellation(conn, a)?,
+        MvtBody::Movement(a) => process_movement(conn, a)?,
+        _ => {
+            warn!("Don't know/care about this type of message yet!");
+            bail!("Unimplemented message type");
+        }
+    }
+    Ok(())
+}
 pub fn process_activation<T: GenericConnection>(conn: &T, a: Activation) -> Result<()> {
     debug!("Processing activation of train {}...", a.train_id);
     let scheds = Schedule::from_select(conn,
         "WHERE uid = $1 AND stp_indicator = $2 AND start_date = $3",
         &[&a.train_uid, &a.schedule_type, &a.schedule_start_date])?;
     if scheds.len() == 0 {
+        debug!("Failed to find a schedule.");
         bail!("Failed to find a schedule (UID {}, start {}, stp_indicator {:?})",
               a.train_uid, a.schedule_start_date, a.schedule_type);
     }
@@ -30,24 +48,67 @@ pub fn process_activation<T: GenericConnection>(conn: &T, a: Activation) -> Resu
     let ways = ScheduleWay::from_select(conn,
                                         "WHERE parent_id = $1",
                                         &[&auth_schedule.id])?;
-    let mut new_ways = vec![];
-    for way in ways {
-        new_ways.push(way.insert_self(conn)?);
-    }
     let train = Train {
         id: -1,
         from_id: auth_schedule.id,
         trust_id: a.train_id,
         date: a.origin_dep_timestamp.date(),
         signalling_id: a.schedule_wtt_id,
-        ways: new_ways
+        ways: vec![]
     };
     let id = train.insert_self(conn)?;
+    let mut new_ways = vec![];
+    for mut way in ways {
+        way.parent_id = None;
+        way.train_id = Some(id);
+        new_ways.push(way.insert_self(conn)?);
+    }
+    conn.execute("UPDATE trains SET ways = $1 WHERE id = $2", &[&new_ways, &id])?;
     debug!("Inserted train as #{}", id);
     Ok(())
 }
 pub fn process_cancellation<T: GenericConnection>(conn: &T, c: Cancellation) -> Result<()> {
     debug!("Processing cancellation of train {}...", c.train_id);
-    let trains = Train::from_select("WHERE train_id = $1", &[&c.train_id])?;
+    let trains = Train::from_select(conn, "WHERE trust_id = $1", &[&c.train_id])?;
+    for train in trains {
+        debug!("Processing train object #{}...", train.id);
+        let ways = ScheduleWay::from_select(conn, "WHERE train_id = $1", &[&train.id])?;
+        for way in ways {
+            conn.execute("DELETE FROM schedule_ways WHERE id = $1", &[&way.id])?;
+        }
+    }
+    debug!("Train cancelled.");
+    Ok(())
+}
+pub fn process_movement<T: GenericConnection>(conn: &T, m: Movement) -> Result<()> {
+    debug!("Processing movement of train {} at STANOX {}...", m.train_id, m.loc_stanox);
+    let trains = Train::from_select(conn, "WHERE trust_id = $1", &[&m.train_id])?;
+    let train = match trains.into_iter().nth(0) {
+        Some(t) => t,
+        None => bail!("No train found for ID {}", m.train_id)
+    };
+    let entries = CorpusEntry::from_select(conn, "WHERE stanox = $1 AND crs IS NOT NULL",
+                                           &[&m.loc_stanox])?;
+    let crs = match entries.into_iter().nth(0) {
+        Some(c) => c.crs.unwrap(),
+        None => bail!("No CRS found for STANOX {}", m.loc_stanox)
+    };
+    debug!("Found CRS: {}", crs);
+    let ways = ScheduleWay::from_select(conn, "WHERE train_id = $1", &[&train.id])?;
+    if ways.len() == 0 {
+        bail!("No ways found for train {}", m.train_id);
+    }
+    for way in ways {
+        let sp = StationPath::from_select(conn, "WHERE id = $1", &[&way.station_path])?;
+        let sp = sp.into_iter().nth(0).expect("Foreign key didn't do its job");
+        if sp.s1 == crs {
+            debug!("Train movement matches way #{}'s start location!", way.id);
+            let way_duration = way.et.signed_duration_since(way.st);
+            let new_end = m.actual_timestamp + way_duration;
+            conn.execute("UPDATE schedule_ways SET st = $1, et = $2 WHERE id = $3",
+                         &[&m.actual_timestamp.time(), &new_end.time(), &way.id])?;
+        }
+    }
+    debug!("Train movement processed.");
     Ok(())
 }
--- osms-nrod/src/main.rs
+++ osms-nrod/src/main.rs
@@ -1,6 +1,11 @@
 extern crate stomp;
 extern crate serde_json;
+extern crate osms_db;
 extern crate ntrod_types;
+extern crate postgres;
+#[macro_use] extern crate log;
+extern crate clap;
+extern crate env_logger;
 
 use stomp::handler::Handler;
 use stomp::session::Session;
@@ -9,34 +14,72 @@ use stomp::subscription::AckOrNack;
 use stomp::connection::*;
 
 use ntrod_types::movements::Records;
-static TOKEN: &str = include_str!("../access_token");
+use postgres::{Connection, TlsMode};
+use clap::{Arg, App};
 
 fn on_message(hdl: &mut LoginHandler, sess: &mut Session<LoginHandler>, fr: &Frame) -> AckOrNack {
     let st = String::from_utf8_lossy(&fr.body);
     let recs: Result<Records, _> = serde_json::from_str(&st);
     match recs {
-        Ok(r) => println!("recs: {:#?}", r),
-        Err(e) => println!("### ERROR ###\nerr: {}\ndata: {}", e, &st)
+        Ok(r) => {
+            for record in r {
+                match osms_db::ntrod::live::process_ntrod_event(&hdl.0, record) {
+                    Err(e) => {
+                        error!("Error processing: {}", e);
+                    },
+                    _ => {}
+                }
+            }
+        },
+        Err(e) => error!("### PARSE ERROR ###\nerr: {}\ndata: {}", e, &st)
     }
 
     AckOrNack::Ack
 }
-struct LoginHandler;
+struct LoginHandler(Connection);
 impl Handler for LoginHandler {
     fn on_connected(&mut self, sess: &mut Session<Self>, frame: &Frame) {
-        println!("Subscription established.");
+        info!("Connection established.");
         sess.subscription("/topic/TRAIN_MVT_ALL_TOC", on_message).start().unwrap();
     }
     fn on_error(&mut self, sess: &mut Session<Self>, frame: &Frame) {
-        println!("Whoops: {}", frame);
+        error!("Whoops: {}", frame);
+    }
+    fn on_disconnected(&mut self, sess: &mut Session<Self>) {
+        warn!("Disconnected.")
     }
 }
 fn main() {
+    env_logger::init().unwrap();
+    let matches = App::new("osms-nrod")
+        .author("eta <http://theta.eu.org>")
+        .about("Receives data from the NTROD feeds and shoves it into the database.")
+        .arg(Arg::with_name("url")
+             .short("l")
+             .value_name("postgresql://USER@IP/DBNAME")
+             .required(true)
+             .takes_value(true)
+             .help("Sets the database URL to use."))
+        .arg(Arg::with_name("user")
+             .short("u")
+             .required(true)
+             .takes_value(true)
+             .help("Login username."))
+        .arg(Arg::with_name("pwd")
+             .short("p")
+             .required(true)
+             .takes_value(true)
+             .help("Login password."))
+        .get_matches();
+    let url = matches.value_of("url").unwrap();
+    let user = matches.value_of("user").unwrap();
+    let pwd = matches.value_of("pwd").unwrap();
+    let conn = Connection::connect(url, TlsMode::None).unwrap();
     let mut cli = stomp::client::<LoginHandler>();
-    cli.session("54.247.175.93", 61618, LoginHandler)
-        .with(Credentials("eeeeeta@users.noreply.github.com", TOKEN))
+    cli.session("54.247.175.93", 61618, LoginHandler(conn))
+        .with(Credentials(user, pwd))
         .with(HeartBeat(5_000, 2_000))
         .start();
-    println!("Running client...");
+    info!("Running client...");
     cli.run()
 }
