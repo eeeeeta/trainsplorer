aadbe57dcdc4b9600010d2e50d6bb1698bf09ae8
eeeeeta <eeeeeta@users.noreply.github.com>
2018-07-06 14:33:15 +0100

    parent_train => parent_sched

--- osms-nrod/src/darwin.rs
+++ osms-nrod/src/darwin.rs
@@ -39,7 +39,7 @@ pub fn get_train_for_rid_uid_ssd<T: GenericConnection>(conn: &T, rid: String, ui
         return Ok(t);
     }
     debug!("Trying to link RID {} (uid {}, start_date {}) to a train...", rid, uid, start_date);
-    let trains = Train::from_select(conn, "WHERE EXISTS(SELECT * FROM schedules WHERE uid = $1 AND id = trains.parent_train) AND date = $2", &[&uid, &start_date])?;
+    let trains = Train::from_select(conn, "WHERE EXISTS(SELECT * FROM schedules WHERE uid = $1 AND id = trains.parent_sched) AND date = $2", &[&uid, &start_date])?;
     if trains.len() > 1 {
         return Err(NrodError::AmbiguousTrains { rid, uid, start_date });
     }
