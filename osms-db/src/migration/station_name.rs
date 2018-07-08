9b1be94d0b1bcb7c078c8d61b4cc0982887ff1eb
eeeeeta <eeeeeta@users.noreply.github.com>
2018-07-08 15:45:12 +0100

    make the migration function actually work

--- osms-db/src/migration/station_name.rs
+++ osms-db/src/migration/station_name.rs
@@ -1,15 +1,15 @@
-use db::*;
 use errors::*;
-use osm::types::*;
 use osm;
 use postgres::transaction::Transaction;
 
 pub fn station_name_func(conn: &Transaction) -> Result<()> {
     debug!("updating station names to comply with new schema");
-    for sta in Station::from_select(conn, "", &[])? {
-        trace!("processing station {}", sta.nr_ref);
-        let name = osm::tiploc_to_readable(conn, &sta.nr_ref)?;
-        conn.execute("UPDATE stations SET name = $1 WHERE id = $2", &[&name, &sta.id])?;
+    for row in &conn.query("SELECT (id, nr_ref) FROM stations", &[])? {
+        let id: i32 = row.get(0);
+        let tiploc: String = row.get(1);
+        trace!("processing station {}", tiploc);
+        let name = osm::tiploc_to_readable(conn, &tiploc)?;
+        conn.execute("UPDATE stations SET name = $1 WHERE id = $2", &[&name, &id])?;
     }
     Ok(())
 }
