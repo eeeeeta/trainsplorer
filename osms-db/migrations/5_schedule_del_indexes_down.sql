e4a1ec319286decb517ad12ca047aa65cab9d216
eeeeeta <eeeeeta@users.noreply.github.com>
2018-07-18 18:11:47 +0100

    DELETE INDEX => DROP INDEX

--- osms-db/migrations/5_schedule_del_indexes_down.sql
+++ osms-db/migrations/5_schedule_del_indexes_down.sql
@@ -1,2 +1,2 @@
-DELETE INDEX IF EXISTS train_movements_parent_mvt;
-DELETE INDEX IF EXISTS train_movements_parent_train;
+DROP INDEX IF EXISTS train_movements_parent_mvt;
+DROP INDEX IF EXISTS train_movements_parent_train;
