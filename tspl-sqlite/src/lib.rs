cd7d2d08d3ba4ca9f6338473e7f0d5e263bb0d7d
eta <hi@theta.eu.org>
2019-08-17 20:29:23 +0100

    sqlite, gcp: add ?, don't needlessly update

--- tspl-sqlite/src/lib.rs
+++ tspl-sqlite/src/lib.rs
@@ -35,7 +35,7 @@ fn busy_cb(n: i32) -> bool {
 fn initialize_connection_without_migrating(conn: &mut Connection) -> ::std::result::Result<(), rusqlite::Error> {
     conn.execute_batch("PRAGMA foreign_keys = ON;")?;
     conn.execute_batch("PRAGMA journal_mode = WAL;")?;
-    conn.busy_handler(Some(busy_cb));
+    conn.busy_handler(Some(busy_cb))?;
     //conn.busy_timeout(std::time::Duration::new(5, 0))?;
     conn.profile(Some(profile_cb));
     Ok(())
