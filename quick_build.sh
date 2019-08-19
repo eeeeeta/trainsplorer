f85fb0368ffe8e4da6b783ea56f1d97381596e9d
eta <hi@theta.eu.org>
2019-08-19 12:26:29 +0100

    k8s, gcp: make tspl-fahrplan a Deployment, fix cloud build
    - tspl-fahrplan is now deployed as a stateless Deployment (hooray!),
      getting its data from GCS.
    - We also now have a readiness probe to avoid serving traffic to pods
      that are still starting up and downloading their databases.

--- quick_build.sh
+++ quick_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "CPU type: $1"
 echo "This is going to be (reasonably) expensive."
-gcloud builds submit --config=cloudbuild.yaml "--machine-type=n1-highcpu-$1" --substitutions "COMMIT_SHA=$(git rev-parse HEAD)" --subsitutions "BRANCH_NAME=quickbuild" .
+gcloud builds submit --config=cloudbuild.yaml "--machine-type=n1-highcpu-$1" --substitutions "COMMIT_SHA=$(git rev-parse HEAD),BRANCH_NAME=quickbuild" .
